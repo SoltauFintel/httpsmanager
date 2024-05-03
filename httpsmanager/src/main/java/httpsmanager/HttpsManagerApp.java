da76a74f15c54d59779efbd725021bfaf0f4175f
Marcus Warm <marcus.warm@x-map.de>
2024-05-03 11:11:09 +0200

    wider domain masks

--- httpsmanager/src/main/java/httpsmanager/HttpsManagerApp.java
+++ httpsmanager/src/main/java/httpsmanager/HttpsManagerApp.java
@@ -28,7 +28,7 @@ import httpsmanager.state.SetStateAction;
 import httpsmanager.state.StateAction;
 
 public class HttpsManagerApp extends RouteDefinitions {
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
     public static AbstractDocker docker;
     public static boolean stateOk = false;
     
